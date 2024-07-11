286974e4012d917f46d8777b635850cf5b2525e9
afisman <afisman@gmail.com>
2024-07-11 13:15:53 +0200

    added navigation

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,13 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button } from 'react-native';
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 const HomeScreen = () => {
+    const navigation = useNavigation();
     return (
         <View>
-            <Text>I am theHomeScreen</Text>
+            <Text>I am the Home Screen</Text>
+            <Button title="Go to chat screen" onPress={() => navigation.navigate("Chat")} />
         </View>
     )
 }
